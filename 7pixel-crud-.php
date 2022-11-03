4936980d16488eb4b85cbc2e7bc7aec60ea776d6
Marco Alvarado <marco.alvarado@jove.com>
2022-11-03 16:32:19 -0300

    test v1.0.1

--- 7pixel-crud-.php
+++ 7pixel-crud-.php
@@ -9,7 +9,7 @@
  * that starts the plugin.
  *
  * @link              https://7pixel.cl
- * @since             1.0.0
+ * @since             1.0.1
  * @package           7pixel_Crud
  *
  * @wordpress-plugin
